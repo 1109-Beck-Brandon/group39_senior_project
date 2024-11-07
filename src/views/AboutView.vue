26362af67072fdbd1d589aa2d50275675050f0f6
beck_brandon <bbeck9802@gmail.com>
2024-11-07 11:39:07 -0800

    Added Vue Router Library and Test Login Page

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar color="surface-variant" title="Cybersecurity Learning Platform"></v-app-bar>
+
+  </v-layout>
+</template>
