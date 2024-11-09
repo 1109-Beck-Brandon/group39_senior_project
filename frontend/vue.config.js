fdd4366896692ff80a17b9bb1431f87c37fe64fc
beck_brandon <bbeck9802@gmail.com>
2024-11-08 16:09:42 -0800

    Revert "Attempt Deployment Fix"
    This reverts commit 6b8dfabb9db32e39e5752944e89ad5369435b76e.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: process.env.NODE_ENV === 'production' ? '/group39_senior_project/front-end/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'group39_senior_project/front-end/' : '/',
   outputDir: 'docs',
 
   pluginOptions: {
@@ -11,4 +10,4 @@ module.exports = defineConfig({
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
   }
-});
+})
